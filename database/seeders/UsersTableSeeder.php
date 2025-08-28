f8d33da9450ed5dbefb28600ae0e9aae562ebe60
haruka ito <>
2025-08-28 19:36:43 +0900

    【ヘッダー】アコーディオンメニューを設置する

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -20,19 +20,19 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             ['username' => 'Atlas一郎',
             'email' => '111@atlas.com',
-            'password' => bcrypt('atlas1')],
+            'password' => bcrypt('atlas111')],
             ['username' => 'Atlas二郎',
             'email' => '222@atlas.com',
-            'password' => bcrypt('atlas2')],
+            'password' => bcrypt('atlas222')],
             ['username' => 'Atlas三郎',
             'email' => '333@atlas.com',
-            'password' => bcrypt('atlas3')],
+            'password' => bcrypt('atlas333')],
             ['username' => 'Atlas四郎',
             'email' => '444@atlas.com',
-            'password' => bcrypt('atlas4')],
+            'password' => bcrypt('atlas444')],
             ['username' => 'Atlas五郎',
             'email' => '555@atlas.com',
-            'password' => bcrypt('atlas5')]
+            'password' => bcrypt('atlas555')]
         ]);
     }
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,8 +1,8 @@
         <div id="head">
             <h1><a href ="{{ url('/top') }}"><img src="images/atlas.png"></a></h1>
-            <div id="">
-                <div id="">
-                    <p>{{ session ('username') }}さん</p>
+            <div id="sub-head">
+                <div id="username">
+                    <p class="white">{{ session ('username') }}さん</p>
                 </div>
                 <div class="accordion">
                     <div class="menu-btn">
@@ -13,9 +13,8 @@
                             <li><a href="/profile">プロフィール</a></li>
                             <li><a href="/logout">ログアウト</a></li>
                         </ul>
-                    </div>
                 </div>
-                <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-                <script src="js/script.js"></script>
             </div>
+            <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+            <script src="js/script.js"></script>
         </div>
