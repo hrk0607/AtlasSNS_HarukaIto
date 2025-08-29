b747dce9cf81e37572530c21282c5738a3982ef3
haruka ito <>
2025-08-29 19:56:42 +0900

    【ヘッダー】アコーディオンメニュー内にログアウト機能を実装する

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -20,19 +20,19 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             ['username' => 'Atlas一郎',
             'email' => '111@atlas.com',
-            'password' => bcrypt('atlas111')],
+            'password' => bcrypt('atlas1')],
             ['username' => 'Atlas二郎',
             'email' => '222@atlas.com',
-            'password' => bcrypt('atlas222')],
+            'password' => bcrypt('atlas2')],
             ['username' => 'Atlas三郎',
             'email' => '333@atlas.com',
-            'password' => bcrypt('atlas333')],
+            'password' => bcrypt('atlas3')],
             ['username' => 'Atlas四郎',
             'email' => '444@atlas.com',
-            'password' => bcrypt('atlas444')],
+            'password' => bcrypt('atlas4')],
             ['username' => 'Atlas五郎',
             'email' => '555@atlas.com',
-            'password' => bcrypt('atlas555')]
+            'password' => bcrypt('atlas5')]
         ]);
     }
 }
