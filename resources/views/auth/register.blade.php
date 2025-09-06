6c2b810f2c9badd58a41e642e28dd295afbbcce3
haruka ito <>
2025-09-06 10:06:56 +0900

    新規登録画面のデザイン調整

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,26 +1,31 @@
 <x-logout-layout>
+    <link rel="stylesheet" href="{{ asset('css/logout.css') }}">
+
     <!-- 適切なURLを入力してください -->
+     <div class="login-box">
 {!! Form::open(['url' => 'register']) !!}
 
-<h2>新規ユーザー登録</h2>
+<p>新規ユーザー登録</p>
 
-{{ Form::label('ユーザー名') }}
-{{ Form::text('username',null,['class' => 'input']) }}
+{{ Form::label('username', 'ユーザー名', ['class' => 'form-label']) }}
+{{ Form::text('username', null, ['class' => 'form-input']) }}
 
-{{ Form::label('メールアドレス') }}
-{{ Form::email('email',null,['class' => 'input']) }}
+{{ Form::label('email', 'メールアドレス', ['class' => 'form-label']) }}
+{{ Form::email('email', null, ['class' => 'form-input']) }}
 
-{{ Form::label('パスワード') }}
-{{ Form::text('password',null,['class' => 'input']) }}
+{{ Form::label('password', 'パスワード', ['class' => 'form-label']) }}
+{{ Form::text('password' ,null, ['class' => 'form-input']) }}
 
-{{ Form::label('パスワード確認') }}
-{{ Form::text('password_confirmation',null,['class' => 'input']) }}
+{{ Form::label('password_confirmation', 'パスワード確認', ['class' => 'form-label']) }}
+{{ Form::text('password_confirmation', null, ['class' => 'form-input']) }}
 
-{{ Form::submit('登録') }}
+<div class="text-end">
+    {{ Form::submit('新規登録', ['class' => 'btn btn-danger btn-lg']) }}
+</div>
 
-<p><a href="login">ログイン画面へ戻る</a></p>
+<p><a class="login-link" href="login">ログイン画面へ戻る</a></p>
 
 {!! Form::close() !!}
-
+</div>
 
 </x-logout-layout>
