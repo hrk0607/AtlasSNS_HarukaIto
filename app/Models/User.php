11fe460a48ae9227ac79128b66c77a7bab1eed6b
haruka ito <>
2025-10-07 22:10:43 +0900

    デザイン最終調整中

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use App\Models\User;
 
 class User extends Authenticatable
 {
@@ -33,15 +32,22 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-public function followings(){
-    return $this->belongsToMany(
-        User::class,'follows','following_id','followed_id'
-    );
-}
-public function followers(){
-    return $this->belongsToMany(
-        User::class,'follows','followed_id','following_id'
-    );
-}
-
+    public function followings()
+    {
+        return $this->belongsToMany(
+            User::class,
+            'follows',
+            'following_id',
+            'followed_id'
+        );
+    }
+    public function followers()
+    {
+        return $this->belongsToMany(
+            User::class,
+            'follows',
+            'followed_id',
+            'following_id'
+        );
+    }
 }
--- resources/views/profiles/user_profile.blade.php
+++ resources/views/profiles/user_profile.blade.php
@@ -3,50 +3,60 @@
 
   <div class="profile-show">
     <div class="top-area">
-      <div class="user-icon">
-        <img src="{{ asset('images/' . $user->icon_image) }}" alt="{{ $user->username }}のアイコン">
-      </div>
       <div class="user-info">
-        <h2>{{ $user->username }}</h2>
-        <p>{{ $user->bio ?? '自己紹介はまだありません。' }}</p>
-      </div>
-      <div class="user-action">
-        @if(Auth::user()->followings->contains($user->id))
+        <div class="user-icon">
+          <img src="{{ asset('images/' . $user->icon_image) }}" alt="{{ $user->username }}のアイコン">
+        </div>
+
+        <div class="user-details">
+          <div class="user-info-row">
+            <label for="username">ユーザー名</label>
+            <p>{{ $user->username }}</p>
+          </div>
+          <div class="user-info-row">
+            <label for="bio">自己紹介</label>
+            <p>{{ $user->bio ?? '自己紹介はまだありません。' }}</p>
+          </div>
+        </div>
+
+        <div class="user-action">
+          @if(Auth::user()->followings->contains($user->id))
           <form action="{{ route('users.unfollow', $user->id) }}" method="POST">
             @csrf
             @method('DELETE')
             <button type="submit" class="btn btn-danger btn-sm follow-btn">フォロー解除</button>
           </form>
-        @else
+          @else
           <form action="{{ route('users.follow', $user->id) }}" method="POST">
             @csrf
             <button type="submit" class="btn btn-info btn-sm text-white follow-btn">フォローする</button>
           </form>
-        @endif
+          @endif
+        </div>
       </div>
 
     </div>
   </div>
 
-<hr class="custom-hr">
+  <hr class="custom-hr">
 
   <ul class="post-list">
     @foreach($userPosts as $post)
-      <li class="post-block">
-        <figure>
-          <div class="user-icon">
-            <img src="{{ asset('images/' . $post->user->icon_image) }}" alt="{{ $post->user->username }}のアイコン">
-          </div>
-        </figure>
-        <div class="post-content">
-          <div class="post-header">
-            <div class="post-name">{{ $post->user->username }}</div>
-            <div class="post-date">{{ $post->created_at->format('Y-m-d H:i') }}</div>
-          </div>
-          <div class="post-text">{{ $post->post }}</div>
+    <li class="post-block">
+      <figure>
+        <div class="user-icon">
+          <img src="{{ asset('images/' . $post->user->icon_image) }}" alt="{{ $post->user->username }}のアイコン">
         </div>
-      </li>
-      <hr class="post-divider">
+      </figure>
+      <div class="post-content">
+        <div class="post-header">
+          <div class="post-name">{{ $post->user->username }}</div>
+          <div class="post-date">{{ $post->created_at->format('Y-m-d H:i') }}</div>
+        </div>
+        <div class="post-text">{{ $post->post }}</div>
+      </div>
+    </li>
+    <hr class="post-divider">
     @endforeach
   </ul>
 
